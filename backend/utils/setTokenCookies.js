a9b630def316d284115d7f911366df99a50117ea
Taha Hamdi <116748061+tahahamdii@users.noreply.github.com>
2024-09-29 02:08:26 +0200

    Update setTokenCookies.js

--- backend/utils/setTokenCookies.js
+++ backend/utils/setTokenCookies.js
@@ -1,29 +1,31 @@
+const setTokenCookies = (res, accessToken, refreshToken, newAccessTokenExp, newRefreshTokenExp) => {
+  // Calculate the expiration time for the access token
+  const accessTokenMaxAge = (newAccessTokenExp - Math.floor(Date.now() / 1000)) * 1000;
+  const refreshTokenMaxAge = (newRefreshTokenExp - Math.floor(Date.now() / 1000)) * 1000;
 
-const setTokenCookies = (res, accessToken,refreshToken,newAccessTokenExp,newRefreshTokenExp ) => {
-    // fetch the time of accessToken
-    const accessTokenMaxAge = (newAccessTokenExp - Math.floor(Date.now()/1000)) * 1000;
-    const refreshTokenMaxAge = (newRefreshTokenExp - Math.floor(Date.now()/1000)) * 1000;
+  // Set cookies for access token
+  res.cookie('accessToken', accessToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production', // Only use secure cookies in production
+    sameSite: 'None', // Allows cookies to be sent with cross-site requests
+    maxAge: accessTokenMaxAge,
+  });
 
-    // set Cookies for accessToken 
-    res.cookie('accessToken',accessToken, {
-        httpOnly: true,
-        secure: true,
-        maxAge: accessTokenMaxAge,
-    });
+  // Set cookies for refresh token
+  res.cookie('refreshToken', refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production', // Only use secure cookies in production
+    sameSite: 'None', // Allows cookies to be sent with cross-site requests
+    maxAge: refreshTokenMaxAge,
+  });
 
-    // set Cookies for refreshToken 
-    res.cookie('refreshToken',refreshToken, {
-        httpOnly: true,
-        secure: true,
-        maxAge: refreshTokenMaxAge,
-    });
+  // Set cookie for is_auth (if needed)
+  res.cookie('is_auth', true, {
+    httpOnly: false,
+    secure: process.env.NODE_ENV === 'production', // Should also be secure in production if used
+    sameSite: 'None', // Allows cookies to be sent with cross-site requests
+    maxAge: refreshTokenMaxAge,
+  });
+};
 
-    // set Cookies for isAuth
-    res.cookie('is_auth',true, {
-        httpOnly: false,
-        secure: false,
-        maxAge: refreshTokenMaxAge,
-    });
-}
-
-export default setTokenCookies
+export default setTokenCookies;
